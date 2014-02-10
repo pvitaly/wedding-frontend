c7c578c8f7a1660ed524f0beca4ec6d00013fbca
pvitaly <pekerv@gmail.com>
2014-02-09 19:22:02 -0500

    FOrmat changes

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('templates');
 
-
 $twig = new Twig_Environment($loader, array(
     'cache' => 'compilation_cache',
     'auto_reload' => TRUE
