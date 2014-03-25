0b532d8e43b0bccbedd8dbc10c8931e7a6762b15
pvitaly <pekerv@gmail.com>
2014-03-25 10:54:03 -0400

    Added validation to forms. Added confirmation modal. Added local vendor files

--- js/wedding.js
+++ js/wedding.js
@@ -5,6 +5,13 @@ var firstGet = true;
 var min_tag_id;
 var tag = "pekerwedding";
 
+var kidsAllowed = false;
+var plusOneAllowed = false;
+var responseFilled = true;
+var plusOneFilled = true;
+var bringingKidsFilled = true;
+var formComplete = true;
+
 function createPhotoElement(photo) {
 
     var innerHtml = $('<img style="padding:2px;">')
@@ -117,83 +124,272 @@ $(document).ready(function() {
         $('#rsvpCode').focus();
     });
 
-    $('#codeSubmitBtn').on('click', function() {
+    $('#rsvpCodeForm').submit(function() {
+        return false;
+    });
 
-        $.ajax({
-            type: "GET",
-            url: "rest/getGuest?uniqueId=" + $("#rsvpCode").val(),
-            error: function(xhr, status, error) {
-                $('#rsvpCodeModal').effect("shake");
-                $("#rsvpCode").val('');
+    $('#rsvpCodeForm').validate({
+        rules: {
+            rsvpCode: {
+                required: true,
+                minlength: 5,
+                maxlength: 5
             }
-        }).done(function(data) {
-
-            // if code not found, display message and exit out of this ajax call
-            if ($.isEmptyObject(data)) {
-                $('#rsvpCodeModal').effect("shake");
-                $("#rsvpCode").val('');
-                return;
+        },
+        messages: {
+            rsvpCode: {
+                required: "We need your invitation code, please."
             }
+        },
+        submitHandler: function() {
+            $.ajax({
+                type: "GET",
+                url: "rest/getGuest?uniqueId=" + $("#rsvpCode").val(),
+                error: function(xhr, status, error) {
+                    $('#rsvpCodeModal').effect("shake");
+                    $("#rsvpCode").val('');
+                }
+            }).done(function(data) {
 
-            if (data.guestStatusEnum !== "WAITING") {
+                // if code not found, display message and exit out of this ajax call
+                if ($.isEmptyObject(data)) {
+                    $('#rsvpCodeModal').effect("shake");
+                    $("#rsvpCode").val('');
+                    return;
+                }
+
+                if (data.guestStatusEnum !== "WAITING") {
+                    $('#rsvpCodeModal').modal('hide');
+                    $("#rsvpCode").val('');
+
+                    $('#rsvpMessage').text("Uh oh, it looks like you have already responded! If you think you made a mistake, give us a call!");
+                    $('#rsvpMessageModal').modal('show');
+
+                    return;
+                }
+
                 $('#rsvpCodeModal').modal('hide');
                 $("#rsvpCode").val('');
 
-                $('#rsvpMessage').text("Uh oh, it looks like you have already responded! If you think you made a mistake, give us a call!");
-                $('#rsvpMessageModal').modal('show');
+                $('#guestName').val(data.guestName);
+                $('#guestCode').val(data.uniqueId);
 
-                return;
-            }
-
-            $('#rsvpCodeModal').modal('hide');
-            $("#rsvpCode").val('');
-
-            $('#guestName').val(data.guestName);
-            $('#guestCode').val(data.uniqueId);
-
-            if (!data.plusOneAllowed) {
-                $('#plusOneDecision').hide();
-            } else {
-                $('#plusOneDecision').show();
-                if (data.plusOneFirstName !== '') {
-                    $('#plusOneName').val(data.plusOneName);
+                if (!data.plusOneAllowed) {
+                    $('#plusOneDecision').hide();
+                    plusOneAllowed = false;
+                } else {
+                    plusOneAllowed = true;
+                    $('#plusOneDecision').show();
+                    if (data.plusOneFirstName !== '') {
+                        $('#plusOneName').val(data.plusOneName);
+                    }
                 }
-            }
 
-            if (!data.kidsAllowed) {
-                $('#partySizeDiv').hide();
-            } else {
-                $('#partySizeDiv').show();
-            }
+                if (!data.kidsAllowed) {
+                    kidsAllowed = false;
+                    $('#partySizeDiv').hide();
+                } else {
+                    kidsAllowed = true;
+                    $('#partySizeDiv').show();
+                }
 
-            if (data.isInWeddingParty) {
-                $("#helpText").text("You must be in the wedding party... select from below.")
-                $('#acceptText').text("I have to come!");
-                $('#declineText').text("Wait... is this optional?!?");
-                $('#declineLabel').attr("id", "acceptLabel2");
-            }
-            $('#rsvpModal').modal('show');
-        });
-
+                if (data.isInWeddingParty) {
+                    $("#helpText").text("You must be in the wedding party... select from below.")
+                    $('#acceptText').text("I have to come!");
+                    $('#declineText').text("Wait... is this optional?!?");
+                    $('#declineLabel').attr("id", "acceptLabel2");
+                }
+                $('#rsvpModal').modal('show');
+            });
+        }
     });
 
+//    $('#codeSubmitBtn').on('click', function() {
+//
+//        $.ajax({
+//            type: "GET",
+//            url: "rest/getGuest?uniqueId=" + $("#rsvpCode").val(),
+//            error: function(xhr, status, error) {
+//                $('#rsvpCodeModal').effect("shake");
+//                $("#rsvpCode").val('');
+//            }
+//        }).done(function(data) {
+//
+//            // if code not found, display message and exit out of this ajax call
+//            if ($.isEmptyObject(data)) {
+//                $('#rsvpCodeModal').effect("shake");
+//                $("#rsvpCode").val('');
+//                return;
+//            }
+//
+//            if (data.guestStatusEnum !== "WAITING") {
+//                $('#rsvpCodeModal').modal('hide');
+//                $("#rsvpCode").val('');
+//
+//                $('#rsvpMessage').text("Uh oh, it looks like you have already responded! If you think you made a mistake, give us a call!");
+//                $('#rsvpMessageModal').modal('show');
+//
+//                return;
+//            }
+//
+//            $('#rsvpCodeModal').modal('hide');
+//            $("#rsvpCode").val('');
+//
+//            $('#guestName').val(data.guestName);
+//            $('#guestCode').val(data.uniqueId);
+//
+//            if (!data.plusOneAllowed) {
+//                $('#plusOneDecision').hide();
+//            } else {
+//                $('#plusOneDecision').show();
+//                if (data.plusOneFirstName !== '') {
+//                    $('#plusOneName').val(data.plusOneName);
+//                }
+//            }
+//
+//            if (!data.kidsAllowed) {
+//                $('#partySizeDiv').hide();
+//            } else {
+//                $('#partySizeDiv').show();
+//            }
+//
+//            if (data.isInWeddingParty) {
+//                $("#helpText").text("You must be in the wedding party... select from below.")
+//                $('#acceptText').text("I have to come!");
+//                $('#declineText').text("Wait... is this optional?!?");
+//                $('#declineLabel').attr("id", "acceptLabel2");
+//            }
+//            $('#rsvpModal').modal('show');
+//        });
+//
+//    });
     $('#rsvpSubmitBtn').on('click', function() {
         var response = $('#response').val();
 
+        if ($('#guestCode').val() === "") {
+            return;
+        }
+
+        if (response === "") {
+            $('#responseError').show();
+            responseFilled = false;
+        } else {
+            if (response === 'decline') {
+                $('#confirmMessageDiv').append('<p>You declined our invitation.');
+                doConfirmModal();
+                return;
+            }
+            responseFilled = true;
+            $('#responseError').hide();
+        }
+
+        if ($('#plusOne').val() === "" && plusOneAllowed) {
+            $('#plusOneError').show();
+            plusOneFilled = false;
+        } else {
+            plusOneFilled = true;
+            $('#plusOneError').hide();
+        }
+
+        if ($('#numberOfKids').val() === "" && kidsAllowed) {
+            $('#kidsError').show();
+            bringingKidsFilled = false;
+        } else {
+            $('#kidsError').hide();
+            bringingKidsFilled = true;
+        }
+
+        if (responseFilled) {
+            formComplete = true;
+            if (plusOneAllowed && !plusOneFilled) {
+                formComplete = false;
+            }
+            if (kidsAllowed && !bringingKidsFilled) {
+                formComplete = false;
+            }
+        } else {
+            formComplete = false;
+        }
+
+        if (!formComplete) {
+            $('#rsvpModal').effect("shake");
+            return;
+        }
+
         if (response === 'accept') {
+            $('#confirmMessageDiv').append('<p>You accepted our invitation.');
+        } else {
+            $('#confirmMessageDiv').append('<p>You declined our invitation.');
+            doConfirmModal();
+            return;
+        }
 
+        if (plusOneAllowed) {
+            if ($('#plusOne').val() === "true") {
+                $('#confirmMessageDiv').append('<p>You are bringing a guest.');
+            } else {
+                $('#confirmMessageDiv').append('<p>You are NOT bringing a guest.');
+            }
+        }
+
+        if (kidsAllowed) {
+            $('#confirmMessageDiv').append('<p>There are a total of ' + $('#numberOfKids').val() + ' in you party.');
+        }
+
+        doConfirmModal();
+
+    });
+    
+    function doConfirmModal() {
+        $('#rsvpModal').modal('hide');
+        $('#rsvpConfirmModal').modal('show');
+    }
+
+    $('#rsvpConfirmBtn').on('click', function() {
+        var response = $('#response').val();
+        if (response === 'accept') {
             acceptRsvp();
-
         } else if (response === 'decline') {
             declineRsvp();
         }
 
     });
 
+    $('#rsvpConfirmGoBackBtn').on('click', function() {
+        $('#confirmMessageDiv').text("");
+        $('#rsvpConfirmModal').modal('hide');
+        $('#rsvpModal').modal('show');
+    });
+
+    function hideResponseError() {
+        responseFilled = true;
+        $('#responseError').hide();
+    }
+
+    function hidePlusOneError() {
+        plusOneFilled = true;
+        $('#plusOneError').hide();
+    }
+
+    function hideKidsError() {
+        $('#kidsError').hide();
+        bringingKidsFilled = true;
+    }
+
     function acceptRsvp() {
         var plusOne = $('#plusOne');
         var numberOfKids = $('#numberOfKids');
         var messageFromGuest = $('#messageFromGuest');
+        var guestName = $('#guestName');
+        var plusOneName = $('#plusOneName');
+
+        if (guestName.val() === "") {
+            guestName.val("null");
+        }
+
+        if (plusOneName.val() === "") {
+            plusOneName.val("null");
+        }
 
         if (plusOne.val() === "") {
             plusOne.val(false);
@@ -209,13 +405,13 @@ $(document).ready(function() {
 
         $.ajax({
             type: "PUT",
-            url: "rest/accept/" + $("#guestCode").val() + "/" + $('#guestName').val() + "/" +
-                    plusOne.val() + "/" + $('#plusOneName').val() + "/" + numberOfKids.val() + "/" + messageFromGuest.val(),
+            url: "rest/accept/" + $("#guestCode").val() + "/" + guestName.val() + "/" +
+                    plusOne.val() + "/" + plusOneName.val() + "/" + numberOfKids.val() + "/" + messageFromGuest.val(),
             error: function(xhr, status, error) {
                 $('#rsvpModal').effect("shake");
             }
         }).done(function(data) {
-            $('#rsvpModal').modal('hide');
+            $('#rsvpConfirmModal').modal('hide');
 
             $('#rsvpMessage').text("Thank you for accepting our invitation. We cannot wait to spend our most special day with you!");
             $('#rsvpMessageModal').modal('show');
@@ -238,7 +434,7 @@ $(document).ready(function() {
                 $('#rsvpModal').effect("shake");
             }
         }).done(function(data) {
-            $('#rsvpModal').modal('hide');
+            $('#rsvpConfirmModal').modal('hide');
 
             $('#rsvpMessage').text("Thank you for taking the time to respond to our invitation. Our regrets you will not be able to make it, but we hope to see you soon.");
             $('#rsvpMessageModal').modal('show');
@@ -256,6 +452,9 @@ $(document).ready(function() {
         $('#messageFromGuest').val("");
         $('#acceptLabel').removeClass('active');
         $('#declineLabel').removeClass('active');
+        $('#acceptLabel').removeClass('greyout');
+        $('#acceptLabel2').removeClass('greyout');
+        $('#declineLabel').removeClass('greyout');
         $('#yesPlusOneLabel').removeClass('active');
         $('#noPlusOneLabel').removeClass('active');
         $('#partySize').val('');
@@ -265,36 +464,93 @@ $(document).ready(function() {
         $('#declineLabel').attr("id", "declineLabel");
         $('#acceptLabel2').attr("id", "declineLabel");
         $('#partySizeDiv').hide();
+        $('#confirmMessageDiv').text("");
         noPlusOne();
+        cleanUpFormBooleans();
+
+    }
+
+    function cleanUpFormBooleans() {
+        kidsAllowed = false;
+        plusOneAllowed = false;
+        responseFilled = true;
+        plusOneFilled = true;
+        bringingKidsFilled = true;
+        formComplete = true;
     }
 
     $('#rsvpModal').on('click', '#acceptLabel', function() {
+        if (!$('#declineLabel').hasClass('greyout')) {
+            $('#declineLabel').addClass('greyout');
+        }
+
+        if (!$('#acceptLabel2').hasClass('greyout')) {
+            $('#acceptLabel2').addClass('greyout');
+        }
+
+        if ($('#acceptLabel').hasClass('greyout')) {
+            $('#acceptLabel').removeClass('greyout');
+        }
+
         $('#response').val("accept");
+        hideResponseError();
     });
 
     $('#rsvpModal').on('click', '#acceptLabel2', function(event) {
         event.stopImmediatePropagation();
+
+        if (!$('#declineLabel').hasClass('greyout')) {
+            $('#declineLabel').addClass('greyout');
+        }
+
+        if (!$('#acceptLabel2').hasClass('greyout')) {
+            $('#acceptLabel2').addClass('greyout');
+        }
+
         $('#response').val("accept");
-        $('#declineLabel').removeClass('active');
+        $('#declineLabel').addClass('greyout');
         $('#acceptLabel').addClass('active');
+        hideResponseError();
     });
 
     $('#rsvpModal').on('click', '#declineLabel', function() {
+        if (!$('#acceptLabel').hasClass('greyout')) {
+            $('#acceptLabel').addClass('greyout');
+        }
+
+        if (!$('#acceptLabel2').hasClass('greyout')) {
+            $('#acceptLabel2').addClass('greyout');
+        }
+
+        if ($('#declineLabel').hasClass('greyout')) {
+            $('#declineLabel').removeClass('greyout');
+        }
+
         $('#response').val("decline");
+        hideResponseError();
     });
 
     $('#rsvpModal').on('change', '#noPlusOne', function() {
         $('#plusOne').val(false);
         noPlusOne();
+        hidePlusOneError();
     });
 
     $('#rsvpModal').on('change', '#yesPlusOne', function() {
         $('#plusOne').val(true);
         yesPlusOne();
+        hidePlusOneError();
     });
 
     $('#rsvpModal').on('change', '#partySize', function() {
         $('#numberOfKids').val($(this).val());
+        if ($(this).val() !== "") {
+            $('#kidsError').hide();
+            bringingKidsFilled = true;
+        } else {
+            $('#kidsError').show();
+            bringingKidsFilled = false;
+        }
     });
 
     $('#rsvpCloseBtn').on('click', function() {
